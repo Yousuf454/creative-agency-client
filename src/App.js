70610fd2b4b50ad66c7b5ed8cc71c9627c94e44b
Yousuf454 <67516997+Yousuf454@users.noreply.github.com>
2020-10-16 21:18:01 +0600

    upload final

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { createContext, useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import AdminAddService from './Component/Admin/AdminAddService/AdminAddService';
+import AdminServiceList from './Component/Admin/AdminServiceList/AdminServiceList';
+import MakeAdmin from './Component/Admin/MakeAdmin/MakeAdmin';
+import SignUp from './Component/Authentication/Login';
+import Order from './Component/Client/Order/Order';
+import Review from './Component/Client/Review/Review';
+import ServiceList from './Component/Client/Service/ServiceList';
+import Home from './Component/HomePage/Home/Home';
+import PrivateRoute from './Component/PrivateRoute/PrivateRoute';
+export const UserContext = createContext();
+function App() {
+
+  const [loggedinUser, setLoggedinUser] = useState({});
+
+    const [isadmin, setisadmin] = useState(false);
+    useEffect(() => {
+      fetch('https://creative-agnecy.herokuapp.com/isAdmin?email=' + loggedinUser.email)
+        .then(response => response.json())
+        .then(data => setisadmin(data));
+    }, [loggedinUser.email])
+    
+  console.log(isadmin);
+  return (
+    <UserContext.Provider value = {[loggedinUser, setLoggedinUser]}>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+
+        <PrivateRoute path="/order/:id">
+          <Order></Order>
+        </PrivateRoute>
+
+        <PrivateRoute path="/order">
+          <Order></Order>
+        </PrivateRoute>
+
+        <Route path="/login">
+          <SignUp></SignUp>
+        </Route>
+
+        <PrivateRoute path="/serviceList">
+         <ServiceList></ServiceList>
+        </PrivateRoute>
+
+        <PrivateRoute path="/review">
+          <Review></Review>
+        </PrivateRoute>
+        
+        <PrivateRoute path="/adminServiceList">
+          {isadmin && <AdminServiceList></AdminServiceList>}
+        </PrivateRoute>
+
+        <PrivateRoute path="/adminaAddService">
+          {isadmin && <AdminAddService></AdminAddService>}
+        </PrivateRoute>
+
+        <PrivateRoute path="/makeAdmin">
+          {isadmin && <MakeAdmin></MakeAdmin>}
+        </PrivateRoute>
+
+      </Switch>
+    </Router>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
